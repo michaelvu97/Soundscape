82231f5e41bd1dc3a2f59040d7dd65a5fdc3c71e
Michael Vu <michael.vu.97@outlook.com>
2019-07-18 11:12:55 -0400

    Add files via upload

---
+++ Direction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soundscape
+{
+	public struct Direction
+	{
+		public byte Dir
+		{
+			get => _direction;
+			private set {
+				if (value > 7)
+					throw new ArgumentOutOfRangeException(nameof(Direction));
+				_direction = value;
+			}
+		}
+		private byte _direction;
+
+		public static readonly Direction FORWARD = new Direction(0);
+		public static readonly Direction FORWARD_LEFT = new Direction(1);
+		public static readonly Direction LEFT = new Direction(2);
+		public static readonly Direction BACKWARD_LEFT = new Direction(3);
+		public static readonly Direction BACKWARD = new Direction(4);
+		public static readonly Direction BACKWARD_RIGHT = new Direction(5);
+		public static readonly Direction RIGHT = new Direction(6);
+		public static readonly Direction FORWARD_RIGHT = new Direction(7);
+
+		public static readonly IReadOnlyCollection<Direction> AllDirections = 
+			new List<Direction>
+			{
+				FORWARD,
+				FORWARD_LEFT,
+				LEFT,
+				BACKWARD_LEFT,
+				BACKWARD,
+				BACKWARD_RIGHT,
+				RIGHT,
+				FORWARD_RIGHT
+			}
+			.AsReadOnly();
+
+		private Direction(byte dir)
+		{
+			if (dir > 7)
+				throw new ArgumentOutOfRangeException(nameof(dir));
+			_direction = dir;
+		}
+	}
+}
---
+++ SoundPackage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Soundscape
+{
+	public sealed class SoundPackage
+	{
+		public string FilePathFormat { get; private set; }
+		
+		public SoundPackage(string filePathFormat)
+		{
+			if (string.IsNullOrWhiteSpace(filePathFormat))
+				throw new ArgumentOutOfRangeException(nameof(filePathFormat));
+
+			FilePathFormat = filePathFormat;
+
+			// Verify that each sound file exists
+			var invalidFileName = 
+				Direction
+				.AllDirections
+				.Select(x => string.Format(FilePathFormat, x.Dir))
+				.FirstOrDefault(x => !File.Exists(filePathFormat));
+
+			if (invalidFileName != null)
+				throw new FieldAccessException(nameof(invalidFileName) + ": " + invalidFileName);
+		}
+
+		public object GetSound(Direction dir)
+		{
+			var filePath = string.Format(FilePathFormat, (int) dir.Dir);
+			
+		}
+	}
+}
