f21c282884de47ce99f44a4949ef468cea332a49
De Almeida,  Paliharalalage <sahandealmeida@gmail.com>
2019-10-02 16:56:04 -0400

    update line length

--- alex/draw_arrow.py
+++ alex/draw_arrow.py
@@ -9,8 +9,9 @@ def drawArrow(screen, theta):
     print("draw arrow with angle" + theta)
     width = screen.get_width()
     height = screen.get_height()
+    r = min(width, height)/2;
     xCentre = width/2
     yCentre = height/2
-    x = width/2 * math.cos(theta)
-    y = height/2 * math.sin(theta)
+    x = xCentre + r*math.cos(theta)
+    y = yCentre + r * math.sin(theta)
     pygame.draw.line(screen, pygame.Color(0,0,0), (xCentre, yCentre), (x,y)) 
