7305401d2a549bae7dfa4b6e83303e264148fa1b
MertTens <amertens114@gmail.com>
2019-10-03 15:55:29 -0400

    changes to draw arrow

--- alex/draw_arrow.py
+++ alex/draw_arrow.py
@@ -5,11 +5,20 @@
 import pygame
 import math
 
+def drawArrowNone(screen):
+    dim = min(screen.get_width(), screen.get_height())
+    
+    pygame.draw.rect(screen, pygame.Color(255, 255, 255), [0, 0, dim, dim]) 
+    pygame.draw.circle(screen, 0, (int(dim/2), int(dim/2)), int(dim/ 2), 4)
+
 def drawArrow(screen, theta):
-    width = screen.get_width()
-    xCentre = width/2
-    yCentre = width/2
-    r = width/2
+    print (theta)
+    dim = min(screen.get_width(), screen.get_height())
+    xCentre = dim/2
+    yCentre = dim/2
+    r = dim/2
     x = xCentre + r*math.cos(theta * math.pi / 180)
     y = yCentre + r * math.sin(-theta * math.pi / 180)
-    pygame.draw.line(screen, pygame.Color(0,0,0), (xCentre, yCentre), (x,y)) 
+   
+    drawArrowNone(screen) 
+    pygame.draw.line(screen, pygame.Color(0,0,0), (xCentre, yCentre), (x,y), 4) 
