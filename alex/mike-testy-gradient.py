d5ffe36b3ed2776d4723fc295d02b6fd886a6c1c
Michael Vu <michael.vu.97@outlook.com>
2019-10-14 19:50:31 -0400

    vectorized gradient test

--- alex/mike-testy-gradient.py
+++ alex/mike-testy-gradient.py
@@ -3,46 +3,43 @@ import math
 
 # Hypothesis functions (for testing)
 f = [
-    lambda t: (math.cosh(t), math.sinh(t)),
-    lambda t: (math.sinh(t) + 5, math.cosh(t))
+    lambda t: np.array([math.cosh(t), math.sinh(t)]),
+    lambda t: np.array([math.sinh(t) + 5, math.cosh(t)]),
+    lambda t: np.array([-math.cosh(t) + 5.4, math.sinh(t)]),
 ]
+
+
+K = len(f)
 
 # Hypothesis function derivatives (for testing)
 f_prime = [
-    lambda t: (math.sinh(t), math.cosh(t)),
-    lambda t: (math.cosh(t), math.sinh(t))
+    lambda t: np.array([math.sinh(t), math.cosh(t)]),
+    lambda t: np.array([math.cosh(t), math.sinh(t)]),
+    lambda t: np.array([-math.sinh(t), math.cosh(t)]),
 ]
 
-def sub(a, b):
-    return (a[0] - b[0], a[1] - b[1])
-
-def mul(c, vec):
-    return (c * vec[0], c * vec[1])
-
-def dot(a,b):
-    return a[0] * b[0] + a[1] * b[1]
-
 # Parameterizations
-t = [0, 0]
+t = np.array([0, 0, 0])
 
-learning_rate = 0.0005
-
+# Careful, if this is too big, it will explode.
+learning_rate = 0.005
 
 while True:
     curr_fprime = [x(t[i]) for i,x in enumerate(f_prime)]
-    # This can be done with a neater sum in the future.
-    curr_other = [
-        sub(f[0](t[0]),f[1](t[1])),
-        sub(f[1](t[1]),f[0](t[0]))
-    ]
+    curr_hypotheses = [x(t[i]) for i,x in enumerate(f)]
 
-    gradient = [
-        dot(curr_fprime[i], curr_other[i]) for i in range(2)
-    ]
+    # For optimization
+    # TODO: cache functional results.
+    sum_point = np.sum(curr_hypotheses, axis=0)
 
-    print("grad: " + str(gradient))
+    grad = lambda p, p_prime: np.dot((K + 1) * p - sum_point, p_prime)
 
-    # TODO use numpy vectorized instead of this jank shit.
-    t = sub(t, mul(learning_rate, gradient))
+    gradient = np.array([
+        grad(curr_fprime[i], curr_hypotheses[i]) for i in range(K)
+    ])
 
-    print("t:" + str(t))
+    # print(gradient)
+
+    t = t - learning_rate * gradient;
+
+    print("Predicted location: " + str(f[0](t[0])))
