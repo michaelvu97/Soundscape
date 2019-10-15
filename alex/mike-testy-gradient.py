326ae1d4c4f03bfacd0d206279930d4f0df56ede
Michael Vu <michael.vu.97@outlook.com>
2019-10-14 21:21:00 -0400

    fixed big ol typo

--- alex/mike-testy-gradient.py
+++ alex/mike-testy-gradient.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math 
+import time
 
 # Hypothesis functions (for testing)
 f = [
@@ -24,7 +25,28 @@ t = np.array([0, 0, 0])
 # Careful, if this is too big, it will explode.
 learning_rate = 0.005
 
-while True:
+early_stopping_threshold = 0.001
+
+def get_predicted_location():
+    return np.mean([f[i](t[i]) for i in range(K)], axis=0)
+
+def get_error(curr_hypotheses):
+    # TODO opt
+    err = 0
+    for i in range(K):
+        for j in range(K):
+            if i == j:
+                continue
+
+            err += np.sum(np.square(curr_hypotheses[i] - curr_hypotheses[j]))
+
+    return err
+
+# TODO
+last_error = 1000000000
+min_error = last_error
+t1 = time.time()
+for i in range (500):
     curr_fprime = [x(t[i]) for i,x in enumerate(f_prime)]
     curr_hypotheses = [x(t[i]) for i,x in enumerate(f)]
 
@@ -35,11 +57,15 @@ while True:
     grad = lambda p, p_prime: np.dot((K + 1) * p - sum_point, p_prime)
 
     gradient = np.array([
-        grad(curr_fprime[i], curr_hypotheses[i]) for i in range(K)
+        grad(curr_hypotheses[i], curr_fprime[i]) for i in range(K)
     ])
 
-    # print(gradient)
-
+    # Update weights.
     t = t - learning_rate * gradient;
 
-    print("Predicted location: " + str(f[0](t[0])))
+    curr_err = get_error(curr_hypotheses)
+t2 = time.time()
+
+print("Predicted location: " + str(get_predicted_location()))
+print("Elapsed time: " + str(t2 - t1) + " seconds")
+
