fcb02fa74b095f9609c0b9d7223395da4224a1d6
Michael Vu <michael.vu.97@outlook.com>
2019-10-16 12:03:58 -0400

    added diagonal generators

--- alex/mike-testy-gradient.py
+++ alex/mike-testy-gradient.py
@@ -1,32 +1,85 @@
 import numpy as np
 import math 
 import time
+import matplotlib.pyplot as plt
+
+speed_of_sound_mps = 330.0 # TODO the correct value.
 
 """
 delta_x is the total horizontal distance between the mics
 y_offset is the vertical distance from the origin.
-delay_ab is the positive (or negative if flipped) amount of time that microphone
-  R lags behind L.
+delay_LR is the positive (or negative if flipped) amount of time that 
+  microphone R lags behind L.
   e.g. if the sound source is very close to the left microphone, delay_LR > 0.
   Note that the sign of this delay is very important.
 
 returns: a paramteric lambda: t -> [x,y]
 """
 def generate_horizontal(delta_x, y_offset, delay_LR):
-    speed_of_sound_mps = 330.0 # todo the correct value.
-    delay_distance = -1 * speed_of_sound_mps * delay_LR # TODO validate the LR dir
+    delay_distance = -1 * speed_of_sound_mps * delay_LR
     a = 0.5 * delay_distance
-
-    # TODO: plus minus
     b = 0.5 * math.sqrt((delta_x ** 2) - (delay_distance ** 2))
 
     print("Horizontal generated: a=" + str(a) + ", b = " + str(b))
     return lambda t: np.array([a * math.cosh(t), b * math.sinh(t) + y_offset])
 
-generate_horizontal(10, 0, 8.27 / 330.0)
-input()
+"""
+delta_y is the total vertical distance between the mics.
+x_offset is the horizontal distance from the origin.
+delay_TB is the positive (or negative if flipped) amount of time that
+  microphone B (botom) lags behind T (top).
+  e.g. if the sound souce is very close to the top microphone, delay_TD > 0.
+  Note that the sign of the delay is very important.
+
+  returns: a parametric lambda: t -> [x,y]
+"""
+def generate_vertical(delta_y, x_offset, delay_TD):
+    delay_distance = speed_of_sound_mps * delay_TD
+    a = 0.5 * delay_distance
+    b = 0.5 * math.sqrt((delta_y ** 2) - (delay_distance ** 2))
+
+    print("Vertical generated: a=" + str(a) + ", b=" + str(b))
+    return lambda t: np.array([b * math.sinh(t) + x_offset, a * math.cosh(t)])
+
+"""
+Generates a hyperbola rotated 45 degrees from horizontal. Must be centered 
+   about the origin.
+delta_pos is the distance between the two microphones.
+delay_BL_TR is the amount of time that microphone TR (top right) lags behind
+  BL (bottom_left). If the sound source is closer to BL, then delay_BL_TR > 0.
+
+returns: a parametric lambda: t ->  [x,y]
+"""
+def generate_diagonal_BL_TR(delta_pos, delay_BL_TR):
+    delay_distance = -1 * speed_of_sound_mps * delay_BL_TR
+    a = 0.5 * delay_distance
+    b = 0.5 * math.sqrt((delta_pos ** 2) - (delay_distance ** 2))
+
+    rotation_coeff = 1/math.sqrt(2) # cos 45 = sin 45 = 1 / sqrt 2
+    print("a: " + str(a) + ", b: " + str(b))
+    # Note that an arbitrary rotation can be achieved by creating a rotation
+    # matrix and transforming the horizontal function by that amount.
+    return lambda t: np.array([
+        rotation_coeff * (a * math.cosh(t) - b * math.sinh(t)),
+        rotation_coeff * (a * math.cosh(t) + b * math.sinh(t))
+    ])
+
+def generate_diagonal_TL_BR(delta_pos, delay_TL_BR):
+    delay_distance = -1 * speed_of_sound_mps * delay_TL_BR
+    a = 0.5 * delay_distance
+    b = 0.5 * math.sqrt((delta_pos ** 2) - (delay_distance ** 2))
+
+    rotation_coeff = 1/math.sqrt(2) # cos 45 = sin 45 = 1 / sqrt 2
+    print("a: " + str(a) + ", b: " + str(b))
+    # Note that an arbitrary rotation can be achieved by creating a rotation
+    # matrix and transforming the horizontal function by that amount.
+    return lambda t: np.array([
+        rotation_coeff * (a * math.cosh(t) + b * math.sinh(t)),
+        rotation_coeff * (-a * math.cosh(t) + b * math.sinh(t))
+    ])
 
 # Hypothesis functions (for testing)
+# TODO: replace with the generated lambdas.
 f = [
     lambda t: np.array([math.cosh(t), math.sinh(t)]),
     lambda t: np.array([math.sinh(t) + 5, math.cosh(t)]),
