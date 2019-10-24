8d67e4e848cb9cf6fdf82fbb2f559eb209e7c537
MertTens <amertens114@gmail.com>
2019-10-24 19:10:51 -0400

    getting stream info for multiple streams at once

--- alex/non_blocking.py
+++ alex/non_blocking.py
@@ -4,34 +4,86 @@ testing out non blocking mode
 
 import pyaudio
 import time
+from utils import get_device_indices
 
-WIDTH = 2
-CHANNELS = 2
+FORMAT  = pyaudio.paInt16 # 16-bit resolution
+CHANNELS = 1
 RATE = 44100
+CHUNK = 4096
 
-def streamCallback(in_data, frame_count, time_info, status):
+"""
+actual callback logic of stream callback
+
+n - name of stream
+in_data - recorded data if input=True for the stream
+frame_count - number of frames for the data
+time_info - dictionary of 
+status - PaCallbackFlags
+"""
+def streamCallback(n, in_data, frame_count, time_info, status):
+    print("stream " +  str(n))
     print(time_info)
     return (in_data, pyaudio.paContinue)
 
-def in_progress(stream):
-    return stream.is_active()
+"""
+generate a callback for some stream
+n - name of stream
+"""
+def streamNCallback(n):
+    return ( lambda in_data, frame_count, time_info, status : 
+        streamCallback(n, in_data, frame_count, time_info, status)
+    )
+
+"""
+check to see if all the streams are active
+
+streams - array of pyaudio stream
+"""
+def in_progress(streams):
+    is_active = True;
+    for stream in streams:
+        is_active = is_active & stream.is_active()
+    return is_active;
+
+def start_streams(streams):
+    for stream in streams:
+        stream.start_stream()
+
+def end_streams(streams):
+    for stream in streams:
+        stream.stop_stream()
+        stream.close()
+"""
+p -  the instance of pyaudio
+index - usb port index
+stream_num - the name of the stream
+"""
+def generate_stream(p, index, stream_num):
+    return  p.open(format=FORMAT,
+                input_device_index=index,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                stream_callback=streamNCallback(stream_num))
 
 if __name__ == "__main__":
 
     p = pyaudio.PyAudio()
+    indices = get_device_indices();
+    streams = [];
+    
+    #the dev indices and sream numbers look off, but the microphones are paired up like this
+    stream1 = generate_stream(p, indices[0], 1)
+    stream4 = generate_stream(p, indices[1], 4)     
 
-    stream = p.open(format=p.get_format_from_width(WIDTH),
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                stream_callback=callback)
+    streams.append(stream1)
+    streams.append(stream4)    
 
-    stream.start_stream()
+    start_streams(streams)
 
-    while in_progress():
+    while in_progress(streams):
         time.sleep(0.1)
-
-    stream.stop_stream()
-    stream.close()
+    
+    end_streams(streams)
 
     p.terminate()
