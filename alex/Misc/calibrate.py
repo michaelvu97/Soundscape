1d2948590ac3821898a337752c1efc42185937bb
De Almeida,  Paliharalalage <sahandealmeida@gmail.com>
2019-11-12 17:23:36 -0500

    most of microsoft article implementation

--- alex/Misc/calibrate.py
+++ alex/Misc/calibrate.py
@@ -1,27 +1,62 @@
 #!/usr/bin/python
 
+'''
+using
+https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/Tashev_MicArraySelfCalibration_ICME_04.pdf
+to calibrate gains of microphones
+'''
 import numpy as np
+from statistics import mean
+import math
 
-#get Energy
+LEARNING_RATE = 0.001;
 
-#divide by expected distance distance
+y_intercept = 0;
+slope = 1;
+'''
+got line of best fit from https://pythonprogramming.net/how-to-program-best-fit-line-machine-learning-tutorial/
+'''
+def compute_line_of_best_fit():
+	slope = compute_slope()
+	y_intercept = compute_y_intecept()
 
-# sampleEnergies: an array of microphone energies <n x 1>
-# sampleDistances: an array representing the distance from the source for each microphone <n x 1>
-def getCalibrationFactors(sampleEnergies, sampleDistances):
-	numEnergies = len(sampleEnergies)
-	numDistances = len(sampleDistances)
-	powers = {}
-	if(numEnergies != numDistances):
-		raise Exception("num energies(" + str(numEnergies) + ") != num distances(" + str(numDistances) + ")")
+def compute_slope():
+	return (((mean(xs)*mean(ys)) - mean(xs*ys)) /
+         ((mean(xs)*mean(xs)) - mean(xs*xs)))
 
-	for i in len(sampleEnergies):
-		energy = sampleEnergies[i]
-		distance = sampleDistances[i]
-		powers.append(energy*(distance * distance))
+def compute_y_intercept(xs, ys):
+ 	return mean(ys) - mean(xs)
 
-	minPower = np.amin(powers);
-	# (1/(x/minPower)) to scale the values around the minimum then find the relative strength 
-	factors = [minPower/x for x in powers]
+def estimate_energy(d):
+	return slope * d + y_intercept
 
-	return factors
+def compute_gain_coefficient(energy, previous_gain, distance):
+	estimate = estimate_energy(distance);
+	gain = previous_gain * math.sqrt(energy/ estimate)
+	return gain;
+
+def normalize_gains(gains):
+	mean_gain = mean(gains)
+	return [x / mean for x in gains]
+
+def compute_normalized_gain_coefficients(energies, previous_gains, distances):
+	gains = [];
+	for i in range(0,n):
+		gains.append(compute_gain_coefficient(energies[i], previous_gains[i], distances[i]))
+	
+	return normalize_gains(gains)
+
+def compute_nth_gen_gain_coefficients(previous_gains, current_gains):
+	new_gains = []
+
+	for i in range(len(current_gains)):
+		new_gains.append(
+			current_gains[i] * LEARNING_RATE 
+			+ previous_gains[i] * (1-LEARNING_RATE)
+		)
+	return new_gains;
+
+
+
+if __name__ == "__main__":
+	print("testing calibrate")
