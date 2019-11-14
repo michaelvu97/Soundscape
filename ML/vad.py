524d3b31603aba12f4d176f72e235ba812e7f92d
Michael Vu <michael.vu.97@outlook.com>
2019-11-14 14:09:57 -0500

    added wav read and windowing

--- ML/vad.py
+++ ML/vad.py
@@ -39,11 +39,47 @@ def FrameToFeatures(frame_time_domain):
 
     return [log_frame_energy, zero_crossing_rate, normalized_autocorrelation_lag_1, first_linear_predictor_coeff, log_linear_predictor_err]
 
-dummy_x_train = [[1,2,-1,2],[1,2,3,4,5,3,3,2]]
-dummy_y_train = [[1],[0]]
+def LabelledFileToTrainingSamples(sound_data, labels, window_size):
+    results_x = []
+    results_y = []
 
-x_train = [FrameToFeatures(x) for x in dummy_x_train]
-y_train = dummy_y_train
+    limit = len(sound_data) - window_size
+    for i in range(limit):
+        samples = sound_data[i:i + window_size]
+        labels = labels[i:i + window_size]
+
+        # Classify this time window as vocals
+        # Considered vocals is over 20 percent of the time is vocals
+        is_speech = np.mean(labels) > 0.2
+
+        results_x.append(samples)
+        results_y.append(is_speech)
+
+    return results_x, results_y
+
+
+rate, wav_data = scipy.io.wavfile.read("room-44100.wav")
+y_labels = np.load("room.npy")
+
+print("data loaded")
+
+# Fix off-by-one errors
+min_length = min(len(y_labels), len(wav_data))
+wav_data = wav_data[:min_length].astype(np.float32)
+y_labels = y_labels[:min_length]
+
+# Transform to windowed
+WINDOW_SIZE = 4410 # 0.1 second window
+x_data_windows, y_data_windows = LabelledFileToTrainingSamples(wav_data, y_labels, WINDOW_SIZE)
+
+print("data windowed")
+
+num_windows = len(y_data_windows)
+
+x_train = [FrameToFeatures(x) for x in x_data_windows]
+y_train = y_data_windows
+
+print("features extracted")
 
 def GetModel():
     inputs = tf.keras.Input(shape=(NUM_FEATURES,), name="features")
@@ -54,6 +90,6 @@ model = GetModel()
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
 print("Fitting model on training data")
-history = model.fit(x_train, y_train, epochs=500)
+history = model.fit(x_train, y_train, epochs=500, validation_split=0.1)
 
 print("History= " + str(history))
