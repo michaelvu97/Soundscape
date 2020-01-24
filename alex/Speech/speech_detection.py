906d6dea3f50f2d8bd255b1c7debc33e0bce7f2c
MertTens <amertens114@gmail.com>
2020-01-23 23:25:30 -0500

    change has_speech to work for array of audios

--- alex/Speech/speech_detection.py
+++ alex/Speech/speech_detection.py
@@ -6,13 +6,22 @@ r = sr.Recognizer()
 #r.energy_threshold = 1000;
 print(r.energy_threshold)
 
-def has_speech(audio):
+def has_speech(audios):
+    if(type(audios) == type([])):
+        outputs = []
+        for a in audios:
+            outputs.append(signal_has_speech(a))
+        return outputs
+    else:
+        return signal_has_speech
+
+def signal_has_speech(audio):
     #return True if recognize(audio) is not None else False
     words = recognize(audio)
     if(words is None):
         return False
-    print("words " + words)
     return True
+
 
 def recognize(audio):
     try:
@@ -58,20 +67,28 @@ if __name__ == "__main__":
         ["btb2.wav", True]
     ];
 
+    audios = []
+    expected = []
     for test in binarytests:
-        
+
         testwav = test[0]
         expectedval = test[1]
-
+        
         with sr.AudioFile(testwav) as source: 
             audio = r.record(source)
-            results = has_speech(audio)
-            if(expectedval == results):
-                print("test " + str(i) + " passed")
-            else: 
-                print("test " + str(i) + " failed")
-                print(testwav)
-                print("expected = " + str(expectedval))
-                print("results = " + str(results))
+            audios.append(audio)
+            expected.append(expectedval)
+
+    results = has_speech(audios)
+    for j in range(0, len(results)):
+        expectedval = expected[j]
+        result = results[j]
+        if(expectedval == result):
+            print("test " + str(i) + " passed")
+        else: 
+            print("test " + str(i) + " failed")
+            print(testwav)
+            print("expected = " + str(expectedval))
+            print("results = " + str(result))
         i = i + 1
 
