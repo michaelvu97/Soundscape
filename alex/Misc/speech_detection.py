1ed50bf3d9a3cd8101a9746080ab42da7787a60d
sahan <sahandealmeida@gmail.com>
2020-03-06 02:26:36 -0500

    removed old code

--- alex/Misc/speech_detection.py
+++ alex/Misc/speech_detection.py
@@ -5,73 +5,12 @@ import time
 import threading
 import sys
 
-r = sr.Recognizer()
-#r.energy_threshold = 1000;
-print(r.energy_threshold)
 
-
-def listen_in_thread(mic, callback):
-    stop_listening = r.listen_in_background(mic, callback)
-    time.sleep(0.4)
-    stop_listening(wait_for_stop=False)
 
 class SpeechDetector:   
-    def __init__ (self, index=None):
-        if index is None:
-            index=7    
-
-        #self.mic = sr.Microphone(device_index=index)
-    
-    def listen(self, callback):
-        #listen_thread = threading.Thread(target=listen_in_thread, args=(self.mic, callback))
-        #listen_thread.start()
-        listen_in_thread(self.mic, callback)
-        
-
-    def is_speech(self, audios):
-        if(type(audios) == type([])):
-            outputs = []
-            for a in audios:
-                outputs.append(self.signal_is_speech(a))
-            return outputs
-        else:
-            return self.signal_is_speech(audios)
-    
-    def signal_is_speech(self, audio):
-        #return True if recognize(audio) is not None else False
-        words = self.recognize(audio)
-        if(words is None):
-            return False
-        return True
-    
-    
-    def recognize(self, audio):
-        #if (type(audio) != type(AudioData)):
-        #audio = r.listen(audio)
-        
-        try:
-            return r.recognize_google(audio);
-        except sr.UnknownValueError:
-            #print("unknown value error")
-            return None
-        except sr.RequestError as e:
-            print("request error")
-            print(e)
-            return None
-    
-def callback(recognizer, audio):
-    try:
-        output = recognizer.recognize_google(audio)
-        print("Google Speech Recognition thinks you said " + output)
-    except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
 if __name__ == "__main__":
     vad =  SpeechDetector();
-
+'''
     print(sr.Microphone.list_microphone_names())
    
     try:
@@ -138,4 +77,4 @@ if __name__ == "__main__":
             print("expected = " + str(expectedval))
             print("results = " + str(result))
         i = i + 1
-
+'''
